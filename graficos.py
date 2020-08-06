b34041db436c422dc05a105d3a8d90faa6e22814
iregazzoli <68082946+iregazzoli@users.noreply.github.com>
2020-08-06 13:50:15 -0300

    Update graficos.py
    aniadi el try/except y que se pueda cambiar el nombre al archivo

--- graficos.py
+++ graficos.py
@@ -2,9 +2,14 @@ import matplotlib.pyplot as plt
 import csv
 import pandas as pd
 
+
+# No es necesario, pero quizas convenga hacer una funcion generica que le pases argumentos como la tabla de datos,
+# el rango, el nombre de los "labels" y asi seguimos, de vuelta, no es necesario, pero para hacerlas mas abstractas conviene -F
 def graficar_temp(data_frame):
-    max_t = data_frame[["Year","Max Temperature"]].groupby("Year",as_index=False).mean() #groupby lo que hace es agrupar la informacion de la columna "Max/Min temp" segun los valores de la columna "year"
-    min_t = data_frame[["Year","Min Temperature"]].groupby("Year",as_index=False).mean() # en criollo seria agarra las temp agrupalas segun el anio y desp hace el promedio
+    max_t = data_frame[["Year", "Max Temperature"]].groupby("Year",
+                                                            as_index=False).mean()  # groupby lo que hace es agrupar la informacion de la columna "Max/Min temp" segun los valores de la columna "year"
+    min_t = data_frame[["Year", "Min Temperature"]].groupby("Year",
+                                                            as_index=False).mean()  # en criollo seria agarra las temp agrupalas segun el anio y desp hace el promedio
     plt.plot(max_t["Year"], max_t["Max Temperature"], "r-", label="Max")
     plt.plot(min_t["Year"], min_t["Min Temperature"], "b-", label="Min")
     plt.plot(max_t["Year"], max_t["Max Temperature"], "ro")
@@ -19,8 +24,9 @@ def graficar_temp(data_frame):
     plt.grid()
     plt.show()
 
+
 def graficar_hum(data_frame):
-    humedad = data_frame[["Year","Relative Humidity"]].groupby("Year",as_index=False).mean()
+    humedad = data_frame[["Year", "Relative Humidity"]].groupby("Year", as_index=False).mean()
     plt.plot(humedad["Year"], humedad["Relative Humidity"], "c-", label="Hum")
     plt.plot(humedad["Year"], humedad["Relative Humidity"], "co")
     plt.ylabel('humedad')
@@ -29,8 +35,9 @@ def graficar_hum(data_frame):
     plt.grid()
     plt.show()
 
+
 def graficar_mm(data_frame):
-    mm = data_frame[["Year","Precipitation"]].groupby("Year",as_index=False).mean()
+    mm = data_frame[["Year", "Precipitation"]].groupby("Year", as_index=False).mean()
     plt.plot(mm["Year"], mm["Precipitation"], "m-", label="mm")
     plt.plot(mm["Year"], mm["Precipitation"], "mo")
     plt.ylabel('mm de lluvia')
@@ -39,8 +46,9 @@ def graficar_mm(data_frame):
     plt.grid()
     plt.show()
 
+
 def graficar_temp_max(data_frame):
-    max_t = data_frame[["Year","Max Temperature"]].groupby("Year",as_index=False).mean()
+    max_t = data_frame[["Year", "Max Temperature"]].groupby("Year", as_index=False).mean()
     plt.plot(max_t["Year"], max_t["Max Temperature"], "r-", label="T-Max")
     plt.plot(max_t["Year"], max_t["Max Temperature"], "ro")
     plt.ylabel('Tempetura_Max')
@@ -50,23 +58,35 @@ def graficar_temp_max(data_frame):
     plt.show()
 
 
-def graficador(): #main
-    data_frame = pd.read_csv("weather.csv",index_col=False) #abre el archivo csv como data frame de pandas, 'index_col=False saca el primer index vacio de la primera fila'
-    data_frame["Year"] = data_frame["Date"].apply(lambda x: x[-4:]) #apply le pasas una funcion y le ejecuta a cada elemento de la columna. (es un map pero para cada fila)
-    print("\n1) Tempeturas promedio de los ultimos 5 años.\n2) Humedad promedio de los ultimos 5 años.\n3) Milímetros máximos de lluvia de los ultimos 5 años.\n4) Temperatura máxima de los ultimos 5 años.")
-    opcion = input("\n¿Qué gráfico desea ver?\n Ingrese el numero de la opción que desea: ")
-    while opcion != "1" and opcion != "2" and opcion != "3" and opcion != "4":
-      print(f'\n¡Error!, la opción "{opcion}" no es válida. Intente nuevamente') 
-      print("\n1) Tempeturas promedio de los ultimos 5 años.\n2) Humedad promedio de los ultimos 5 años.\n3) Milímetros máximos de lluvia de los ultimos 5 años.\n4) Temperatura máxima de los ultimos 5 años.")
-      opcion = input("\n¿Qué gráfico desea ver?\n Ingrese el numero de la opción que desea: ")  
-    
-    if opcion == "1":
-        graficar_temp(data_frame)
-    elif opcion == "2":
-        graficar_hum(data_frame)
-    elif opcion == "3":
-        graficar_mm(data_frame)
-    elif opcion == "4":
-        graficar_temp_max(data_frame)
+def graficador():  # main
+    # Quizas darle al usuario la opcion de dar otro nombre de archivo? -F
+    # Y si el archivo no existe, muy probablemente todo esto rompa, aviso. -F
+    print("A continuacion ingrese el nombre del archivo csv en el siguiente formato: nombre.csv, asegurece que el "
+          "archivo este en el mismo directorio que el programa tormenta")
+    archivo_usuario = input(
+        "Ingrese el nombre del archivo:")  # RECORDAR COLOCAR EL NOMBRE DEL ARCHIVO EN LA LINEA DE ABAJO!!
+    try:
+        data_frame = pd.read_csv(archivo_usuario, index_col=False)  # abre el archivo csv como data frame de pandas, 'index_col=False saca el primer index vacio de la primera fila'
+        data_frame["Year"] = data_frame["Date"].apply(lambda x: x[-4:])  # apply le pasas una funcion y le ejecuta a cada elemento de la columna. (es un map pero para cada fila)
+        print(
+            "\n1) Tempeturas promedio de los ultimos 5 años.\n2) Humedad promedio de los ultimos 5 años.\n3) Milímetros máximos de lluvia de los ultimos 5 años.\n4) Temperatura máxima de los ultimos 5 años.")
+        opcion = input("\n¿Qué gráfico desea ver?\n Ingrese el numero de la opción que desea: ")
+        while opcion != "1" and opcion != "2" and opcion != "3" and opcion != "4":
+            print(f'\n¡Error!, la opción "{opcion}" no es válida. Intente nuevamente')
+            print(
+                "\n1) Tempeturas promedio de los ultimos 5 años.\n2) Humedad promedio de los ultimos 5 años.\n3) Milímetros máximos de lluvia de los ultimos 5 años.\n4) Temperatura máxima de los ultimos 5 años.")
+            opcion = input("\n¿Qué gráfico desea ver?\n Ingrese el numero de la opción que desea: ")
 
+        if opcion == "1":
+            graficar_temp(data_frame)
+        elif opcion == "2":
+            graficar_hum(data_frame)
+        elif opcion == "3":
+            graficar_mm(data_frame)
+        elif opcion == "4":
+            graficar_temp_max(data_frame)
+    except FileNotFoundError as error:
+        print("No se pudo encontrar el archivo ingresado, asegurese que este"
+        "bien escrito o que se encuentre en el repositorio,")
+        print(error)
 
